fbd8506b597688b654a95a7b2c59da345d9566df
billwk254 <billkihungu51@gmail.com>
2023-08-02 02:35:52 +0300

    A script to extract data from a log file

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,26 +1,17 @@
 #!/usr/bin/env ruby
 
-def parse_log_line(log_line)
-  sender = log_line[/from:([^\]]+)/, 1]
-  receiver = log_line[/to:([^\]]+)/, 1]
-  flags = log_line[/flags:([^\]]+)/, 1]
-  "#{sender},#{receiver},#{flags}"
+def extract_data(log_line)
+  sender = log_line[/from:(.*?)\]/, 1]
+  receiver = log_line[/to:(.*?)\]/, 1]
+  flags = log_line[/flags:(.*?)\]/, 1]
+  [sender, receiver, flags].join(',')
 end
 
 if ARGV.length != 1
-  puts "Usage: #{$PROGRAM_NAME} <log_file>"
+  puts "Usage: #{$PROGRAM_NAME} <log_line>"
   exit 1
 end
 
-log_file_path = ARGV[0]
-
-begin
-  File.foreach(log_file_path) do |line|
-    if line.include?("SMS")
-      parsed_data = parse_log_line(line)
-      puts parsed_data
-    end
-  end
-rescue Errno::ENOENT
-  puts "Error: Log file not found."
-end
+log_line = ARGV[0]
+parsed_data = extract_data(log_line)
+puts parsed_data
