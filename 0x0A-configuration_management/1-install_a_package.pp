88f297dccef4745488365166d4782de2c76d4918
billwk254 <billkihungu51@gmail.com>
2023-10-03 22:04:37 +0300

    install flask using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,22 +1,6 @@
 # Puppet manifest to install Flask version 2.1.0 and its dependencies using pip3
 
-class { 'python::pip':
-  version => 'latest',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
-
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'Install Flask 2.1.0':
-  command     => 'pip3 install Flask==2.1.0',
-  path        => '/usr/local/bin',
-  refreshonly => true,
-}
-
-exec { 'Install werkzeug 2.1.1':
-  command     => 'pip3 install werkzeug==2.1.1',
-  path        => '/usr/local/bin',
-  refreshonly => true,
-}
-
