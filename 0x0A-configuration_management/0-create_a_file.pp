630183256e841683d83da8b3616089fb76c18e24
billwk254 <billkihungu51@gmail.com>
2023-10-03 17:47:30 +0300

    Using Puppet, create a file in /tmp.

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Puppet manifest to create a file in /tmp/school
 
 file { '/tmp/school':
-  ensure   => file,
-  mode     => '0744',
-  owner    => 'www-data',
-  group    => 'www-data',
-  content  => 'I love Puppet',
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
