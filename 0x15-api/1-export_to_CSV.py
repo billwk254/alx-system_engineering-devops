39af0ba02352565c97332a318b67c4e1e179ece7
billwk254 <billkihungu51@gmail.com>
2023-10-24 00:06:35 +0300

    Python script to export data in the CSV format.

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -14,14 +14,14 @@ def export_user_data(user_id):
     user_data = requests.get(user_url).json()
     todos_data = requests.get(todos_url).json()
 
-    with open(f"{user_id}_todos.csv", mode="w", newline="") as csv_file:
+    with open(f"{user_id}.csv", mode="w", newline="") as csv_file:
         csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow(["User ID", "Username", "Completed", "Title"])
+        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
 
         for todo in todos_data:
             csv_writer.writerow([user_id, user_data["username"], todo["completed"], todo["title"]])
 
-if __name__ == "__main":
+if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python3 script.py <user_id>")
         sys.exit(1)
