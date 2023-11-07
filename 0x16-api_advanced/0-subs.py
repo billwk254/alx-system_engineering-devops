c98176de45fc0fca71e36da4009858e23b7e3e37
billwk254 <billkihungu51@gmail.com>
2023-11-07 14:00:33 +0300

    Changes to 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
+
+
 """
 0-subs
 """
+
+
 import requests
+
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
-    
+    Returns the number of subscribers for a given subreddit.
     Args:
         subreddit (str): The name of the subreddit to query.
 
     Returns:
-        int: The number of subscribers for the subreddit, or 0 if the subreddit is invalid.
+        int: The number of subscribers for the subreddit
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'AlxBot/1.0 (by wamboo_bazenga)'}
@@ -25,6 +29,7 @@ def number_of_subscribers(subreddit):
         return subscribers
     else:
         return 0
+
 
 if __name__ == '__main__':
     import sys
